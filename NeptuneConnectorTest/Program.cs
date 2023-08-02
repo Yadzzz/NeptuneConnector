b81de4cbd53059ca1f6567a7a81d1a43ee65bbbc
Yad <60503097+Yadzzz@users.noreply.github.com>
2023-08-02 11:28:14 +0200

    9879879878979

--- NeptuneConnectorTest/Program.cs
+++ NeptuneConnectorTest/Program.cs
@@ -8,8 +8,16 @@ namespace NeptuneConnectorTest
 
         static void Main(string[] args)
         {
+            ConnectorConfiguration config = new ConnectorConfiguration
+            {
+                Ip = "127.0.0.1",
+                Port = 30000,
+                AutoConnect = true,
+                KeepConnectionAlive = true
+            };
+
             connector = new Connector(
-                "127.0.0.1", 30000, 
+                config,
                 "2a60314f-5daf-4d9d-8b03-5d1973a36277", 
                 "393be0e4-5aa5-4b5e-b915-3242085ea087", 
                 "5ef32396-0ace-4133-bd84-32f44bb703ce",
